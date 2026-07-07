9076b27be519bcd1c06c50aca11ec3d5173119ba
thabataagomide <thabataamandagomide@gmail.com>
2026-07-07 20:48:03 -0300

    update

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -75,12 +75,7 @@ export interface FileRoutesById {
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
   fullPaths:
-    | '/'
-    | '/assistir'
-    | '/biblioteca'
-    | '/buscar'
-    | '/perfil'
-    | '/sitemap.xml'
+    '/' | '/assistir' | '/biblioteca' | '/buscar' | '/perfil' | '/sitemap.xml'
   fileRoutesByTo: FileRoutesByTo
   to: '/' | '/assistir' | '/biblioteca' | '/buscar' | '/perfil' | '/sitemap.xml'
   id:
@@ -160,3 +155,13 @@ const rootRouteChildren: RootRouteChildren = {
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
   ._addFileTypes<FileRouteTypes>()
+
+import type { getRouter } from './router.tsx'
+import type { startInstance } from './start.ts'
+declare module '@tanstack/react-start' {
+  interface Register {
+    ssr: true
+    router: Awaited<ReturnType<typeof getRouter>>
+    config: Awaited<ReturnType<typeof startInstance.getOptions>>
+  }
+}
